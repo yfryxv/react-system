80b72317591819fd8b0f0deacfd27fda98f33f31
JeanP13R <jeanpiervilaarizapana@gmail.com>
2024-11-14 11:43:55 -0500

    Descripción de los cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        si lo lees eres senior en react
       </p>
     </>
   )
